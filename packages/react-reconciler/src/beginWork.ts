43120487b8e98584d07551363861bd725429e2c7
phlpsong <philsongfei@gmail.com>
2023-07-03 21:21:25 +0800

    feat: reconciler implement

---
+++ packages/react-reconciler/src/beginWork.ts
@@ -0,0 +1,7 @@
+// 递归中的 递 阶段
+
+import { FiberNode } from './fiber';
+
+export const beginWork = (fiber: FiberNode) => {
+	// 比较 返回 子fiberNode
+};
---
+++ packages/react-reconciler/src/fiber.ts
@@ -0,0 +1,47 @@
+import { Key, Props, Ref } from 'shared/ReactTypes';
+import { WorkTag } from './workTags';
+import { Flags, NoFlags } from './fiberFlags';
+
+export class FiberNode {
+	type: any;
+	tag: WorkTag;
+	pendingProps: Props;
+	key: Key;
+	stateNode: any;
+	ref: Ref;
+
+	return: FiberNode | null;
+	sibling: FiberNode | null;
+	child: FiberNode | null;
+	index: number;
+
+	memoizedProps: Props | null;
+	alternate: FiberNode | null;
+	flags: Flags;
+
+	constructor(tag: WorkTag, pendingProps: Props, key: Key) {
+		// instance property
+		this.tag = tag;
+		this.key = key;
+		// HostComponent <div> -> div DOM
+		this.stateNode = null;
+		// FunctionComponent () => {}
+		this.type = null;
+
+		// tree stucture
+		// point to parent fiberNode
+		this.return = null;
+		this.sibling = null;
+		this.child = null;
+		this.index = 0;
+
+		this.ref = null;
+
+		this.pendingProps = pendingProps;
+		this.memoizedProps = null;
+
+		this.alternate = null;
+		// side effects
+		this.flags = NoFlags;
+	}
+}
