503cc8eb9704b77cb1c1bf8594a937cd540d8580
phlpsong <philsongfei@gmail.com>
2023-08-17 22:00:34 +0800

    feat: 17.1 sync update

---
+++ demos/test-fc/main.ts
@@ -0,0 +1,39 @@
+import './style.css';
+
+const button = document.querySelector('button');
+const root = document.querySelector('#root');
+
+interface Work {
+	count: number;
+}
+
+const workList: Work[] = [];
+
+function schedule() {
+	const curWork = workList.pop();
+	if (curWork) {
+		perform(curWork);
+	}
+}
+
+function perform(work: Work) {
+	while (work.count) {
+		work.count--;
+		insertSpan('0');
+	}
+	schedule();
+}
+
+function insertSpan(content) {
+	const span = document.createElement('span');
+	span.innerText = content;
+	root?.appendChild(span);
+}
+
+button &&
+	(button.onclick = () => {
+		workList.unshift({
+			count: 100
+		});
+		schedule();
+	});
