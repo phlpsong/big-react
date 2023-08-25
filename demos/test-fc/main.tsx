536ccc3a72b288eabe0ee8cc8ad144138449609a
phlpsong <philsongfei@gmail.com>
2023-08-25 23:45:41 +0800

    feat: 18.3 concurrent update

--- demos/test-fc/main.tsx
+++ demos/test-fc/main.tsx
@@ -1,20 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import ReactDOM from 'react-noop-renderer';
+import { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
 
 function App() {
+	const [num, update] = useState(100);
 	return (
-		<>
-			<Child />
-			<div>hello world</div>
-		</>
+		<ul onClick={() => update(50)}>
+			{new Array(num).fill(0).map((_, i) => {
+				return <Child key={i}>{i}</Child>;
+			})}
+		</ul>
 	);
 }
 
-function Child() {
-	return 'Child';
+function Child({ children }) {
+	const now = performance.now();
+	while (performance.now() - now < 4) {}
+	return <li>{children}</li>;
 }
 
-const root = ReactDOM.createRoot();
-root.render(<App />);
+const root = ReactDOM.createRoot(document.querySelector('#root'));
 
-window.root = root;
+root.render(<App />);
