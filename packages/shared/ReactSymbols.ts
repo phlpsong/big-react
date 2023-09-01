0b90093f9a1e045493ad3ae5dd14a508e568edde
phlpsong <philsongfei@gmail.com>
2023-09-01 22:15:25 +0800

    feat: useContext structure

--- packages/shared/ReactSymbols.ts
+++ packages/shared/ReactSymbols.ts
@@ -4,6 +4,10 @@ export const REACT_ELEMENT_TYPE = supportSymbol
 	? Symbol.for('react.element')
 	: 0xeac7;
 
-export const REACT_FRAGMENT_TYPE = supportSymbol
-	? Symbol.for('react.fragment')
-	: 0xeac;
+export const REACT_CONTEXT_TYPE = supportSymbol
+	? Symbol.for('react.context')
+	: 0xeacc;
+
+export const REACT_PROVIDER_TYPE = supportSymbol
+	? Symbol.for('react.provider')
+	: 0xea2;
